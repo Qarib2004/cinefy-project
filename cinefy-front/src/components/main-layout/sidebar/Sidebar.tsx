e551ed3db2e7975b32696c70b46c917a08298c9e
Qarib2004 <garibalisultanov@gmail.com>
2025-08-22 17:43:15 +0400

    cinefy

--- cinefy-front/src/components/main-layout/sidebar/Sidebar.tsx
+++ cinefy-front/src/components/main-layout/sidebar/Sidebar.tsx
@@ -1,4 +1,4 @@
-import { FC } from 'react'
+import { FC, useState } from 'react'
 
 import styles from './Sidebar.module.scss'
 import Logo from './logo/Logo'
@@ -6,15 +6,31 @@ import MenuContainer from './navigation/MenuContainer'
 import Subscribe from './subscribe/Subscribe'
 
 const Sidebar: FC = () => {
-	return (
-		<div className={styles.wrapper}>
-			<div className={styles.sidebar}>
-				<Logo />
-				<MenuContainer />
-				<Subscribe />
-			</div>
-		</div>
-	)
+  const [isOpen, setIsOpen] = useState(false) 
+
+  const toggleSidebar = () => {
+    setIsOpen(prev => !prev)
+  }
+
+  return (
+    <>
+       <button
+        className="md:hidden fixed top-4 left-4 z-50 p-2 bg-gray-200 rounded"
+        onClick={toggleSidebar}
+      >
+        {isOpen ? 'Close' : 'Menu'}
+      </button>
+
+      <div className={`${styles.wrapper} ${isOpen ? styles.open : ''}`}>
+        <div className={styles.sidebar}>
+          <Logo />
+          <MenuContainer />
+          <Subscribe />
+        </div>
+        {isOpen && <div   className="fixed inset-0 bg-black/30 z-40 md:hidden" onClick={toggleSidebar} />}
+      </div>
+    </>
+  )
 }
 
 export default Sidebar
