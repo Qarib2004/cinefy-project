ff47fe03822b7bbafa366c89f43be6264eb7b261
Qarib2004 <garibalisultanov@gmail.com>
2025-09-04 23:33:53 +0400

    upgrade sidebar

--- cinefy-front/src/components/main-layout/MainLayout.tsx
+++ cinefy-front/src/components/main-layout/MainLayout.tsx
@@ -1,47 +1,21 @@
-// MainLayout.tsx
-import { FC, PropsWithChildren, useState } from "react";
+import { FC, PropsWithChildren } from "react";
 import styles from './MainLayout.module.scss'
 import Header from "./header/Header";
 import Sidebar from "./sidebar/Sidebar";
 
+
+
+
 const MainLayout: FC<PropsWithChildren<unknown>> = ({children}) => {
-    const [isSidebarOpen, setIsSidebarOpen] = useState(false); // По умолчанию закрыт на мобильных
-
-    const toggleSidebar = () => {
-        setIsSidebarOpen(!isSidebarOpen);
-    };
-
     return (
-        <div className={`${styles.layout} ${isSidebarOpen ? styles.sidebarOpen : ''}`}>
+        <div className={styles.layout}>
             <div className="flex-1">
-                <Header  />
-                <Sidebar isOpen={isSidebarOpen} onClose={() => setIsSidebarOpen(false)} />
+                <Header/>
+                <Sidebar/>
                 <main>{children}</main>
             </div>
         </div>
+
     )
 }
-
 export default MainLayout
-
-// import { FC, PropsWithChildren } from "react";
-// import styles from './MainLayout.module.scss'
-// import Header from "./header/Header";
-// import Sidebar from "./sidebar/Sidebar";
-
-
-
-
-// const MainLayout: FC<PropsWithChildren<unknown>> = ({children}) => {
-//     return (
-//         <div className={styles.layout}>
-//             <div className="flex-1">
-//                 <Header/>
-//                 <Sidebar/>
-//                 <main>{children}</main>
-//             </div>
-//         </div>
-
-//     )
-// }
-// export default MainLayout
--- cinefy-front/src/components/main-layout/sidebar/Sidebar.tsx
+++ cinefy-front/src/components/main-layout/sidebar/Sidebar.tsx
@@ -1,145 +1,41 @@
 'use client'
+import { FC, useState } from 'react'
 
-import { FC } from 'react'
 import styles from './Sidebar.module.scss'
 import Logo from './logo/Logo'
 import MenuContainer from './navigation/MenuContainer'
 import Subscribe from './subscribe/Subscribe'
 
-interface SidebarProps {
-  isOpen: boolean;
-  onClose: () => void;
-}
+const Sidebar: FC = () => {
+  const [isOpen, setIsOpen] = useState(false)
 
-const Sidebar: FC<SidebarProps> = ({ isOpen, onClose }) => {
-	return (
-		<>
-			{/* Кнопка меню - только на мобильных */}
-			<button
-				className={`
-					fixed top-4 left-4 z-[1000] p-3 
-					bg-red-500 text-white rounded shadow-lg 
-					transition-transform duration-200
-					md:hidden
-					hover:bg-red-600
-					active:scale-95
-				`}
-				onClick={onClose}
-			>
-				{isOpen ? 'Close' : 'Menu'}
-			</button>
+  const toggleSidebar = () => setIsOpen(prev => !prev)
 
-			{/* Сайдбар */}
-			<div className={`${styles.wrapper} ${isOpen ? styles.open : ''}`}>
-				<div className={styles.sidebar}>
-					<Logo />
-					<MenuContainer />
-					<Subscribe />
-				</div>
-			</div>
+  return (
+    <>
+      <button
+        className="md:hidden fixed top-4 left-4 z-[1000] p-3 bg-blue-600 text-white rounded shadow-lg"
+        onClick={toggleSidebar}
+      >
+        {isOpen ? 'Close' : 'Menu'}
+      </button>
 
-			{/* Overlay - только на мобильных когда сайдбар открыт */}
-			{isOpen && (
-				<div
-					className='fixed inset-0 bg-black/30 z-[998] md:hidden'
-					onClick={onClose}
-				/>
-			)}
-		</>
-	)
+      <div className={`${styles.wrapper} ${isOpen ? 'open' : ''}`}>
+        <div className={styles.sidebar}>
+          <Logo />
+          <MenuContainer />
+          <Subscribe />
+        </div>
+
+        {isOpen && (
+          <div
+            className="fixed inset-0 bg-black/30 z-[998] md:hidden"
+            onClick={toggleSidebar}
+          />
+        )}
+      </div>
+    </>
+  )
 }
 
 export default Sidebar
-
-// 'use client'
-
-// import { FC, useState } from 'react'
-
-// import styles from './Sidebar.module.scss'
-// import Logo from './logo/Logo'
-// import MenuContainer from './navigation/MenuContainer'
-// import Subscribe from './subscribe/Subscribe'
-
-
-
-// const Sidebar: FC = () => {
-// 	const [isOpen, setIsOpen] = useState(false)
-
-// 	const toggleSidebar = () => setIsOpen(prev => !prev)
-
-// 	return (
-// 		<>
-// 			<button
-// 				className={`
-//     fixed top-4 left-4 z-[1000] p-3 
-//     bg-red-500 text-white rounded shadow-lg 
-//     transition-transform duration-200
-//     md:hidden
-//     hover:bg-red-600
-//     active:scale-95
-//   `}
-// 				onClick={toggleSidebar}
-// 			>
-// 				{isOpen ? 'Close' : 'Menu'}
-// 			</button>
-
-// 			<div className={`${styles.wrapper} ${isOpen ? styles.open : ''}`}>
-// 				<div className={styles.sidebar}>
-// 					<Logo />
-// 					<MenuContainer />
-// 					<Subscribe />
-// 				</div>
-// 			</div>
-
-// 			{isOpen && (
-// 				<div
-// 					className='fixed inset-0 bg-black/30 z-[998] md:hidden'
-// 					onClick={toggleSidebar}
-// 				/>
-// 			)}
-// 		</>
-// 	)
-// }
-
-// export default Sidebar
-// 'use client'
-// import { FC, useState } from 'react'
-
-// import styles from './Sidebar.module.scss'
-// import Logo from './logo/Logo'
-// import MenuContainer from './navigation/MenuContainer'
-// import Subscribe from './subscribe/Subscribe'
-
-// const Sidebar: FC = () => {
-//   const [isOpen, setIsOpen] = useState(false)
-
-//   const toggleSidebar = () => setIsOpen(prev => !prev)
-
-//   return (
-//     <>
-//       <button
-//         className="md:hidden fixed top-4 left-4 z-[1000] p-3 bg-blue-600 text-white rounded shadow-lg"
-//         onClick={toggleSidebar}
-//       >
-//         {isOpen ? 'Close' : 'Menu'}
-//       </button>
-
-//       <div className={`${styles.wrapper} ${isOpen ? 'open' : ''}`}>
-//         <div className={styles.sidebar}>
-//           <Logo />
-//           <MenuContainer />
-//           <Subscribe />
-//         </div>
-
-//         {isOpen && (
-//           <div
-//             className="fixed inset-0 bg-black/30 z-[998] md:hidden"
-//             onClick={toggleSidebar}
-//           />
-//         )}
-//       </div>
-//     </>
-//   )
-// }
-
-// export default Sidebar
