93e913d4572a09ed95e00c8f2db37ad46a3a9de1
Qarib2004 <garibalisultanov@gmail.com>
2025-09-04 23:15:42 +0400

    upgrade sidebar

--- cinefy-front/src/components/main-layout/MainLayout.tsx
+++ cinefy-front/src/components/main-layout/MainLayout.tsx
@@ -4,23 +4,37 @@ import styles from './MainLayout.module.scss'
 import Header from "./header/Header";
 import Sidebar from "./sidebar/Sidebar";
 
-
-
-
-
-
-
-
 const MainLayout: FC<PropsWithChildren<unknown>> = ({children}) => {
     return (
         <div className={styles.layout}>
-            <div className="flex-1">
-                <Header/>
-                <Sidebar/>
-                <main>{children}</main>
-            </div>
+            <Sidebar/>
+            
+            <Header/>
+            <main>{children}</main>
         </div>
-
     )
 }
+
 export default MainLayout
+
+// import { FC, PropsWithChildren } from "react";
+// import styles from './MainLayout.module.scss'
+// import Header from "./header/Header";
+// import Sidebar from "./sidebar/Sidebar";
+
+
+
+
+// const MainLayout: FC<PropsWithChildren<unknown>> = ({children}) => {
+//     return (
+//         <div className={styles.layout}>
+//             <div className="flex-1">
+//                 <Header/>
+//                 <Sidebar/>
+//                 <main>{children}</main>
+//             </div>
+//         </div>
+
+//     )
+// }
+// export default MainLayout
